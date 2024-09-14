2e4c45e08cf6b002cc6fdfa939fbb769c83d0d33
minho <minhob38@gmail.com>
2024-09-15 01:53:31 +0900

    feat: Set database

---
+++ backend/src/main/java/com/minho/backend/api/auth/domain/entity/User.java
@@ -0,0 +1,45 @@
+package com.minho.backend.api.auth.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import java.util.Date;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String email;
+
+  private String password;
+
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(name = "created_at")
+  private Date createdAt;
+
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(name = "updated_at")
+  private Date updatedAt;
+
+  @Override
+  public String toString() {
+    return "id: " + this.getId()
+        + "\nemail: " + this.getEmail()
+        + "\npassword: " + this.getPassword()
+        + "\ncreatedAt: " + this.getCreatedAt()
+        + "\nupdatedAt: " + this.getUpdatedAt();
+  }
+}
