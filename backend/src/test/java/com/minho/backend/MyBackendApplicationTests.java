74075fc71fc3603f3bbe024537895ffa2ff9e44b
minho <minhob38@gmail.com>
2024-09-14 18:30:54 +0900

    chore: Initialize backend

---
+++ backend/src/test/java/com/minho/backend/MyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minho.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
