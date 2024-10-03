d5b9fa8c2b593147c4ddbaa02ff59ca266f0a3c3
minho <minhob38@gmail.com>
2024-10-03 20:23:01 +0900

    feat: Implement data seeding

---
+++ backend/src/main/java/com/minho/backend/api/common/DataInitializer.java
@@ -0,0 +1,30 @@
+package com.minho.backend.api.common;
+
+import com.minho.backend.api.auth.domain.entity.User;
+import com.minho.backend.api.auth.domain.port.AuthPersistencePort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DataInitializer implements CommandLineRunner {
+
+	private final AuthPersistencePort authPersistenceAdapter;
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("### Initializing Data ###");
+
+		String password = bCryptPasswordEncoder.encode("qwerasdf");
+		User user = User.builder()
+				.email("gaeul@gmail.com")
+				.password(password)
+				.build();
+		this.authPersistenceAdapter.createUser(user);
+
+		System.out.println("### Initialized Data ###");
+	}
+}
