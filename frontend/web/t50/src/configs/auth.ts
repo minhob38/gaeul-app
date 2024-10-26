a56fa175ed6067972b2a4542455b4a03676151c5
minho <minhob38@gmail.com>
2024-10-26 18:03:42 +0900

    refactor: Refactor code

--- frontend/web/t50/src/configs/auth.ts
+++ frontend/web/t50/src/configs/auth.ts
@@ -1,2 +1,3 @@
 export const LOCAL_STORAGE_ACCESS_TOKEN_KEY = "access-token";
 export const ACCESS_TOKEN_HEADER = "access_token";
+export const AUTH_ENDPOINTS = ["/signin", "signup"];
--- frontend/web/t50/src/configs/axios-config.ts
+++ frontend/web/t50/src/configs/axios-config.ts
@@ -1,6 +1,6 @@
 import { checkInitialAuth } from "@apis/functions";
 import axios from "axios";
-import { ACCESS_TOKEN_HEADER, LOCAL_STORAGE_ACCESS_TOKEN_KEY } from "./auth";
+import { ACCESS_TOKEN_HEADER, AUTH_ENDPOINTS, LOCAL_STORAGE_ACCESS_TOKEN_KEY } from "./auth";
 
 // export const API_SERVER_ADDRESS = "http://3.39.126.180";
 // export const API_SERVER_ADDRESS =
@@ -19,10 +19,8 @@ const instance = axios.create({
 
 instance.interceptors.request.use(
   async (config: any) => {
-    const isAuthApi = config.url.includes("/signin") || config.url.includes("/signup");
+    const isAuthApi = AUTH_ENDPOINTS.includes(config.url);
     const accessToken = localStorage.getItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY);
-
-    console.log(accessToken);
 
     if (!isAuthApi && accessToken) {
       config.headers[ACCESS_TOKEN_HEADER] = `Bearer ${accessToken}`;
