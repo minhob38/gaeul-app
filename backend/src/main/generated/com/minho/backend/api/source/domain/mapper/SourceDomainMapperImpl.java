22ba93269e02b876c5e973fea252e498ad6c40c4
minho <minhob38@gmail.com>
2024-11-15 23:49:58 +0900

    chore: Build mapper

--- backend/src/main/generated/com/minho/backend/api/source/domain/mapper/SourceDomainMapperImpl.java
+++ backend/src/main/generated/com/minho/backend/api/source/domain/mapper/SourceDomainMapperImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-10-27T20:49:33+0900",
+    date = "2024-11-15T21:47:43+0900",
     comments = "version: 1.6.1, compiler: javac, environment: Java 17.0.8.1 (Amazon.com Inc.)"
 )
 @Component
